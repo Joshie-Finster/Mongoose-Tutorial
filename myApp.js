f6c462c4c7c20b55fb13c51f0237f3a051c1b8a9
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-19 09:19:33 +0000

    find and update

--- myApp.js
+++ myApp.js
@@ -77,8 +77,11 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+  Person.findByIdAndUpdate({name:personName}, {age:ageToSet}, {new: true}, (err,updatedDoc) =>{
+    if (err) return console.log(err);
+    done(null,updatedDoc);
+  })
 
-  done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
